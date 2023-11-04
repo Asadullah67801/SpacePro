fa292ce8d093d27f59a82687ca828fc2e4100f0e
Asadullah67801 <itsasad9669@gmail.com>
2023-11-04 17:49:36 +0500

    added

---
+++ js/script.js
@@ -0,0 +1,59 @@
+const Btn = document.getElementById("menubtn");
+const Flow = document.getElementById("Flow");
+const menu = document.getElementById("mobile-menu");
+const counters = document.querySelectorAll(".counter");
+let scrollStarted = false;
+
+Btn.addEventListener("click", navToggle);
+document.addEventListener("scroll", scrollPage);
+
+function navToggle() {
+  Btn.classList.toggle("open");
+  Flow.classList.toggle("Flow-show");
+  document.body.classList.toggle("stop-scrolling");
+  menu.classList.toggle("show-menu");
+}
+
+function scrollPage() {
+  const scrollPos = window.scrollY;
+
+  if (scrollPos > 100 && !scrollStarted) {
+    countUp();
+    scrollStarted = true;
+  } else if (scrollPos < 100 && scrollStarted) {
+    reset();
+    scrollStarted = false;
+  }
+}
+
+function countUp() {
+  counters.forEach((counter) => {
+    counter.innerText = "0";
+
+    const updateCounter = () => {
+      // Get count target
+      const target = +counter.getAttribute("data-target");
+      // Get current counter value
+      const c = +counter.innerText;
+
+      // Create an increment
+      const increment = target / 100;
+
+      // If counter is less than target, add increment
+      if (c < target) {
+        // Round up and set counter value
+        counter.innerText = `${Math.ceil(c + increment)}`;
+
+        setTimeout(updateCounter, 75);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCounter();
+  });
+}
+
+function reset() {
+  counters.forEach((counter) => (counter.innerHTML = "0"));
+}
